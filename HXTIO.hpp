16b335e7d2145273de271c0626490eefb0907492
Seth Parker <csparker247@gmail.com>
2017-08-01 17:14:34 -0400

    Templated 2D and 3D array

--- HXTIO.hpp
+++ HXTIO.hpp
@@ -1,9 +1,117 @@
+#pragma once
 // HEXITEC IO
 #include <fstream>
 #include <vector>
 
 namespace libhxt
 {
+
+/**
+ * @class Array2D
+ * @brief Generic 2D array template
+ */
+template <typename T>
+class Array2D
+{
+public:
+    /** Storage container alias */
+    using Container = std::vector<T>;
+    /** Container index type */
+    using Index_t = typename Container::size_type;
+
+    /** @brief Constructor with dimensions */
+    Array2D(Index_t rows, Index_t cols) : rows_{rows}, cols_{cols}
+    {
+        data_.resize(rows * cols);
+    }
+
+    /** @brief Element access */
+    virtual T& operator()(Index_t y, Index_t x)
+    {
+        return data_.at((y * cols_) + x);
+    }
+
+    /** @overload operator()(Index_t y, Index_t x) */
+    virtual const T& operator()(Index_t y, Index_t x) const
+    {
+        return data_.at((y * cols_) + x);
+    }
+
+protected:
+    /** Default constructor */
+    Array2D() = default;
+
+    /** Constructor for initialization from existing STL container */
+    Array2D(
+        Index_t r,
+        Index_t c,
+        typename Container::iterator first,
+        typename Container::iterator last)
+        : rows_{r}, cols_{c}, data_{first, last}
+    {
+    }
+
+    /** Number of rows */
+    Index_t rows_{0};
+    /** Number of columns */
+    Index_t cols_{0};
+    /** Data storage */
+    Container data_;
+};
+
+/**
+ * @class Array3D
+ * @brief Generic 3D array template
+ */
+template <typename T>
+class Array3D : public Array2D<T>
+{
+public:
+    /** Storage container alias */
+    using Container = std::vector<T>;
+    /** Container index type */
+    using Index_t = typename Container::size_type;
+
+    /** @brief Constructor with dimensions */
+    Array3D(Index_t pages, Index_t rows, Index_t cols) : pages_{pages}
+    {
+        this->rows_ = rows;
+        this->cols_ = cols;
+        this->data_.resize(pages * rows * cols);
+    }
+
+    /** @brief Element access */
+    T& operator()(Index_t z, Index_t y, Index_t x)
+    {
+        return this->data_.at(
+            (z * this->cols_ * this->rows_) + (y * this->cols_) + x);
+    }
+
+    /** @overload operator()(Index_t z, Index_t y, Index_t x) */
+    const T& operator()(Index_t z, Index_t y, Index_t x) const
+    {
+        return this->data_.at(
+            (z * this->cols_ * this->rows_) + (y * this->cols_) + x);
+    }
+
+    /** @brief Get page of array */
+    Array2D<T> page(Index_t z)
+    {
+        auto b = std::next(this->data_.begin(), z * this->cols_ * this->rows_);
+        auto e =
+            std::next(this->data_.begin(), (z + 1) * this->cols_ * this->rows_);
+        return Array2D<T>(this->rows_, this->cols_, b, e);
+    }
+
+    /** Deleted operator that doesn't make sense for 3D array */
+    T& operator()(Index_t y, Index_t x) = delete;
+
+    /** @overload T& operator()(Index_t y, Index_t x) */
+    const T& operator()(Index_t y, Index_t x) const = delete;
+
+private:
+    Index_t pages_{0};
+};
 
 class HXT
 {
